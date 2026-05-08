6248eee18a3928f35252931f9de59df264a65f3b
Keith Richardson <k.richardson.UrbanKingdomMgmt@gmail.com>
2026-05-08 14:32:30 -0700

    V1.03.044 Move mobile toast above bottom nav

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const APP_SHELL_CACHE = "production-crew-shell-v1.03.043";
+const APP_SHELL_CACHE = "production-crew-shell-v1.03.044";
 const APP_SHELL_ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=V1.03.043-combined-event-visibility",
-  "./app.js?v=V1.03.043-combined-event-visibility",
+  "./styles.css?v=V1.03.044-mobile-toast-clearance",
+  "./app.js?v=V1.03.044-mobile-toast-clearance",
   "./manifest.webmanifest",
   "./assets/app-icon.svg"
 ];
