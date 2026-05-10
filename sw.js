6b74e82baad27e5cfed710ae4a31dc5252338af1
Keith Richardson <k.richardson.UrbanKingdomMgmt@gmail.com>
2026-05-09 20:33:50 -0700

    V1.03.086 Fit mobile events view

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const APP_SHELL_CACHE = "production-crew-shell-v1.03.085";
+const APP_SHELL_CACHE = "production-crew-shell-v1.03.086";
 const APP_SHELL_ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=V1.03.085-clean-mobile-messages",
-  "./app.js?v=V1.03.085-clean-mobile-messages",
+  "./styles.css?v=V1.03.086-fit-mobile-events",
+  "./app.js?v=V1.03.086-fit-mobile-events",
   "./manifest.webmanifest",
   "./assets/app-icon.svg"
 ];
