92fe6bd5c0a3173010da1ca3257b7108251d50ce
Keith Richardson <k.richardson.UrbanKingdomMgmt@gmail.com>
2026-05-10 13:11:45 -0700

    V1.04.006 Grow sidebar background

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const APP_SHELL_CACHE = "production-crew-shell-v1.04.005";
+const APP_SHELL_CACHE = "production-crew-shell-v1.04.006";
 const APP_SHELL_ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=V1.04.005-soften-mobile-logout",
-  "./app.js?v=V1.04.005-soften-mobile-logout",
+  "./styles.css?v=V1.04.006-grow-sidebar-background",
+  "./app.js?v=V1.04.006-grow-sidebar-background",
   "./manifest.webmanifest",
   "./assets/app-icon.svg"
 ];
